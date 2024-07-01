91bf0da9f77af4c1316f52294028e74770e9f679
PragyaJain006 <projectpragya3@gmail.com>
2024-07-01 16:32:16 +0530

    Ques1

---
+++ src/features/LetterChanges.js
@@ -0,0 +1,36 @@
+// Input: "hello*3"
+// Output: Ifmmp*3
+let input= "fun times!"
+function letterChanges(str){
+    const words=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    let out=[]
+    let noWord=[]
+ for(i of str){
+     for(let j=0; j<words.length;j++){
+     if(words[j]===i){
+         i= words[j+1]
+         out.push(i)
+         break;
+    }else if(!words.includes(i)){
+        noWord.push(i)
+        break;
+    }
+    
+ }
+
+}
+let output=[]
+for(i of out){
+    if(i==="a"||i==="e"||i==="i"||i==="o"||i==="u"){
+      i= i.toUpperCase();
+      output.push(i);
+    }else{
+        output.push(i);
+    }
+    
+
+}
+return output.concat(noWord).join("");
+
+}
+console.log(letterChanges(input))
