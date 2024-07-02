b41d59180582e329bf45a18de65daaf53857b656
PragyaJain006 <projectpragya3@gmail.com>
2024-07-02 20:51:23 +0530

    "updated"

--- src/features/ArrayFlatten.js
+++ src/features/ArrayFlatten.js
@@ -5,6 +5,17 @@ var flat = function (arr, n) {
         if (Array.isArray(item) && n > 0) {
             // Recursively flatten the array and reduce the depth by 1
             flatArr = flatArr.concat(flat(item, n - 1));
+            // we have used concat here not push as we have to combine arrays into one 
+            //concat returns us the new array , whereas push changes the original array
+            //push is used to merge non-array elements.
+            // let num=[1,2,3] let nums=[4,5]
+            //let number= []
+            //number.push(num) //[[1,2,3]]
+            //let num=[1,2,3]
+// let n=[4,5]
+// let number= []
+// const a=number.concat(num,n)
+// console.log(a)//[1,2,3,4,5]
         } else {
             // Add non-array elements to flatArr
             flatArr.push(item);
