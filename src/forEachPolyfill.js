f44d1954bb772e8eefdcb73db05e9dd111790747
PragyaJain006 <projectpragya3@gmail.com>
2024-07-16 11:32:56 +0530

    "pollyfil for forEach"

---
+++ src/forEachPolyfill.js
@@ -0,0 +1,12 @@
+const arr=[1,2,3,4,5]
+function cb(ele){
+    console.log(ele)
+}
+Array.prototype.myForEach= function(cb){
+    // console.log(this,"this") // [1,2,3,4,5]
+    for(let i=0; i<this.length; i++){
+        cb(this[i],i,this)
+    }
+}
+
+arr.myForEach(cb)
